d78c32030e5852a3fad8b0710d147fa2d8f69ef4
David M <davidmatiasdm19@gmail.com>
2025-11-18 15:45:13 -0600

    Tercer commit

---
+++ src/views/GameGato.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div class="flex items-center justify-center">
+        <h1 class="text-4xl font-medium text-black">
+            Jugando al Gato
+        </h1>
+    </div>
+
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+onMounted(() => {
+    console.log('Iniciemos la partida')
+})
+
+</script>
---
+++ src/views/Lobby.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div class="flex items-center justify-center">
+        <h1 class="text-4xl font-medium text-black">
+            Estas preparado...
+        </h1>
+    </div>
+
+    <div class="space-y-6">
+        <button class="text-white font-medium bg-green-600 p-6 w-full h-[12rem]">
+            Crear partida
+        </button>
+
+        <button class="text-white font-medium bg-blue-600 p-6 w-full h-[12rem]">
+            Unirse a una partida
+        </button>
+    </div>
+
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+onMounted(() => {
+    console.log('Hola wapo.')
+})
+
+</script>
