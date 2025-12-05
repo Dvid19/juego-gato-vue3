1f5d530430fb32b6dd667666a8c738e94a828b6a
Dvid19 <davidmatiasdm19@gmail.com>
2025-12-05 08:04:10 -0600

    C13

--- src/services/GameGato/gato.service.js
+++ src/services/GameGato/gato.service.js
@@ -1,3 +1,4 @@
 import api from "../api.service";
 
 export const createGameGato = async () => await api.post('/games'); 
+export const joinGameGato = async (code) => await api.post(`/games/join/${code}`)
--- src/views/GameGato/GatoUnirsePartidaView.vue
+++ src/views/GameGato/GatoUnirsePartidaView.vue
@@ -6,7 +6,7 @@
             Ingresa el codigo para unirte a la partida
         </p>
 
-        <form @submit.prevent=""
+        <form @submit.prevent="join"
             class="flex flex-col gap-3"
         >
             <input type="text" v-model="code" placeholder="Codigo de partida" class="min-w-xs border-2 py-2 px-4 rounded-md text-center"
@@ -23,12 +23,36 @@
 
 <script setup>
 import { ref } from 'vue'
+import echo from "../../resources/echo";
+import { joinGameGato } from '../../services/GameGato/gato.service';
 
 const code = ref('');
 
+onMounted(() => {
+
+})
+
+async function join(){
+    if(!code.value) return
+
+    try {
+        const response = await joinGameGato(code.value);
+        console.log(response)
+    } catch (e) {
+        alert(e.message);
+    }
+}
+
+// Escuchar WebSocket
+function listenToChannel(gameId) {
+  console.log('Conectadonos a la partida de Gato..')
+  echo.join(`game.${conversationId}`).listen("GameJoined", (e) => {
+    console.log("data de reverb: ", e);
+  });
+}
+
 function convertirMayus(){
     code.value = code.value.toUpperCase();
-    console.log(code.value);
 }
 
 </script>
